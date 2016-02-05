c0accd445cad876e25144e8cc3589bea3be5cee6
Avanger2512 <den2512@ukr.net>
2016-02-05 12:35:18 +0200

    upd

--- src/assets/js/_coffee.js
+++ src/assets/js/_coffee.js
@@ -2063,12 +2063,6 @@ $(document).on("change", ".pill-group .pill input", function(e) {
   }
 });
 
-$(function() {
-  return $(".price-block_collapse .price-block__header").on("click", function() {
-    return $(this).closest(".price-block").toggleClass("price-block_collapse_open");
-  });
-});
-
 var postLocationWidthFix;
 
 postLocationWidthFix = function() {
@@ -2114,6 +2108,12 @@ $(window).resize(postLocationWidthFix);
 $(document).on("shown.bs.tab", postLocationWidthFix);
 
 postLocationWidthFix();
+
+$(function() {
+  return $(".price-block_collapse .price-block__header").on("click", function() {
+    return $(this).closest(".price-block").toggleClass("price-block_collapse_open");
+  });
+});
 
 var smallCardInit;
 
